cefbcd2a379a4fc086bf429d1dcd9b671d42020e
buckket <buckket@cock.li>
2016-01-04 15:01:19 +0100

    updated bvg_people

--- lg_bvg.py
+++ lg_bvg.py
@@ -9,7 +9,7 @@ import settings
 
 
 bvg_accounts = ['232867314', '234688983', '234689386']
-bvg_people = ['Yesica', 'Yesi', 'Ines', 'Nico', 'André', 'René', 'Chris', 'Petra', 'Klaus', 'Sabine', 'Feierabend']
+bvg_people = ['Yesica', 'Yesi', 'Ines', 'Nico', 'André', 'René', 'Chris', 'Petra', 'Klaus', 'Sabine', 'Dirk', 'Feierabend']
 
 
 class BVGListener(StreamListener):
